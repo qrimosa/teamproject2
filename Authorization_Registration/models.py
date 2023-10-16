7b9e791d2765416ac97bcd14a2956a455a9fb727
Marat Iskhakov <93031480+qrimosa@users.noreply.github.com>
2023-10-16 20:47:19 +0300

    Co-authored-by: Andrii Bystrov <AndriiBystrov@users.noreply.github.com>

--- Authorization_Registration/models.py
+++ Authorization_Registration/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Auth(models.Model):
+    phone=models.IntegerField()
+    email=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    login=models.CharField(max_length=255)
